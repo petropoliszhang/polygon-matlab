5fc983011d95aad8097f1ea851ecf9ee0e5b2060
Jean C. Ragusa <jean.ragusa@tamu.edu>
2013-07-31 17:18:33 -0500

    more centroid

--- pwld_diffusion/my_centroid.m
+++ pwld_diffusion/my_centroid.m
@@ -1,14 +1,25 @@
-function [Cx,Cy] = my_centroid( x , y )
+function [C] = my_centroid( vert, centroid_opt )
 % Computes the centroid of a polygon
+% global centroid_opt
 
-% aux var
-aux = x.*y([2:end 1]) - y.*x([2:end 1]);
+switch centroid_opt
+    case{1}
+        C=mean(vert);
+    case{2}
+        % aux var
+        x=vert(:,1);
+        y=vert(:,2);
+        aux = x.*y([2:end 1]) - y.*x([2:end 1]);
+        
+        % polygon's signed area
+        signed_area=0.5* sum(aux);
+        
+        % centroid coordinates
+        C(1) = sum( ( x + x([2:end 1]) ).*aux ) / (6*signed_area);
+        C(2) = sum( ( y + y([2:end 1]) ).*aux ) / (6*signed_area);
+    otherwise
+        error('centroid option unknown');
+end
 
-% polygon's signed area
-signed_area=0.5* sum(aux);
-
-% centroid coordinates
-Cx = sum( ( x + x([2:end 1]) ).*aux ) / (6*signed_area);
-Cy = sum( ( y + y([2:end 1]) ).*aux ) / (6*signed_area);
-
+return
 end
--- pwld_diffusion/refine_geom_v2.m
+++ pwld_diffusion/refine_geom_v2.m
@@ -115,18 +115,17 @@ for k=1:n_cell_to_ref
     is_in=inpolygon(centroid(1),centroid(2),v(:,1),v(:,2));
     if(~is_in)
         % try true centroid
-        [cx,cy] = my_centroid(v(:,1),v(:,2));
-        is_in2=inpolygon(cx,cy,v(:,1),v(:,2));
+        [c_] = my_centroid(v,2);
+        is_in2=inpolygon(c_(1),c_(2),v(:,1),v(:,2));
         if(~is_in2)
             iel
             g
             v
             centroid
-            [cx cy]
+            c_
             error('centroid is not in polygon');
         else
-            centroid(1) = cx;
-            centroid(2) = cy;
+            centroid = c_;
         end
     end
     i=1; new_pt1 = (v(i,:)+v(i+1,:))/2;
