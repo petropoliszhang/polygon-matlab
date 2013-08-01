4693c8803ebf8b34da4075e3dd2e53e82167d1b7
Jean C. Ragusa <jean.ragusa@tamu.edu>
2013-08-01 17:21:42 -0500

    linear res driver

--- pwld_diffusion/linear_random_driver.m
+++ pwld_diffusion/linear_random_driver.m
@@ -11,8 +11,21 @@ geo = 'random_quad_mesh_L100_n30_a0.1.txt'   ;
 geo = 'random_quad_mesh_L100_n30_a0.33.txt'  ;
 geo = 'random_quad_mesh_L100_n3_a0.25.txt'   ;
 % geo = 'random_quad_mesh_L100_n50_a0.33.txt'  ;
-   
+geo = 'shestakov_poly_mesh_L1_nc4_a0.15.txt';
 
+% august 1 below:
+% geo = 'random_quad_mesh_L1_n16_a0.65.txt' ; % one small oscillation, goes away with use of <0 det
+% geo = 'random_quad_mesh_L1_n16_a0.5.txt' ;  % good 
+% geo = 'random_quad_mesh_L1_n16_a0.66.txt' ; % good with use of <0 det
+% geo = 'z_mesh_quad_L1_n20_a0.05.txt' ;  % good
+% geo = 'shestakov_quad_L1_nc4_a0.15.txt'; % good, some wiggles, does not improve with <0 det
+% geo = 'shestakov_quad_L1_nc4_a0.25.txt'; % good
+% geo = 'misha_quad_L1_n4.txt'; % not working
+% geo = 'random_quad_mesh_L1_n3_a0.txt' ; % good, must use <0 det
+
+% geo = 'random_poly_mesh_L1_n15_a0.95.txt'; % good
+% geo = 'random_poly_mesh_L1_n15_a0.99.txt'; % good
+% 
 if(strcmp(geo,''))
     data.geofile = '';
 else
@@ -32,7 +45,43 @@ data.logi_plot           = true;
 data.logi_plot_err_i     = false;
 data.generate_vtk_output = true;
 
-result_basename = 'results\reg_quad_';
+result_dir = 'results\';
+% is quad or poly mesh
+found_poly = strfind(geo,'poly');
+found_quad = strfind(geo,'quad');
+if( ( isempty(found_poly) &&  isempty(found_quad) )|| ...
+    (~isempty(found_poly) && ~isempty(found_quad) ) )
+    error('impossible mesh type');
+elseif ( ~isempty(found_poly) && isempty(found_quad) )
+    mesh_type='poly';
+elseif ( isempty(found_poly) && ~isempty(found_quad) )
+    mesh_type='quad';
+else
+    error('unknown mesh type');
+end
+% is rand, shes, or z-mesh ?
+found_rand = strfind(geo,'random');
+if(isempty(found_rand))
+    found_shes = strfind(geo,'shestakov');
+    if(isempty(found_shes))
+        found_z = strfind(geo,'z_mesh');
+        if(isempty(found_z))
+            found_misha = strfind(geo,'misha');
+            if(isempty(found_misha))
+                error('unknown mesh');
+            else
+                result_basename = strcat(result_dir,'mish_',mesh_type,'_');
+            end
+        else
+            result_basename = strcat(result_dir,'zzzz_',mesh_type,'_');
+        end
+    else
+        result_basename = strcat(result_dir,'shes_',mesh_type,'_');
+    end
+else
+    result_basename = strcat(result_dir,'rand_',mesh_type,'_');
+end
+    
 if(data.logi_mms)
     result_basename = sprintf('%s%s%c',result_basename,'mms_',int2str(data.mms_type));
 else
