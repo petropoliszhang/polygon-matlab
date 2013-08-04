1abd2ea065d92d8d25399b9519d106fe71431233
Jean C. Ragusa <jean.ragusa@tamu.edu>
2013-08-04 16:32:26 -0500

    updates

--- pwld_diffusion/testing_driver.m
+++ pwld_diffusion/testing_driver.m
@@ -3,9 +3,18 @@ disp('Running script that drives pwld');
 
 global verbose
 verbose = true;
+verbose = false;
 
 % data.geofile = '';
 geo = 'shestakov_quad_L1_nc8_emb4_a0.1.txt';
+geo = 'random_poly_mesh_L1_n64_a0.965.txt' ;
+geo = 'random_poly_mesh_L1_n64_a0.9.txt'
+geo = 'z_mesh_quad_L1_n9_a0.05.txt'
+geo = 'z_mesh_quad_L1_n6_a0.05.txt'
+geo = 'z_mesh_quad_L1_n5_a0.05.txt'
+geo = 'shestakov_poly_mesh_L1_nc4_a0.02.txt';
+geo = 'z_mesh_poly_L1_n5_a0.05.txt';
+
 data.geofile = sprintf('%s%s','..\geom_codes\figs\',geo);
 
 data.logi_mms = true;
@@ -19,9 +28,11 @@ data.ref_threshold  = 0; % 0=uniform refinement
 
 data.logi_plot           = true;
 data.logi_plot_err_i     = false;
-data.generate_vtk_output = false;
+data.generate_vtk_output = true;
 
-result_basename = 'results\reg_quad_';
+result_basename = 'results\z_mesh_poly_n5_a0.05_';
+% result_basename = 'results\toto_';
+
 if(data.logi_mms)
     result_basename = sprintf('%s%s%c',result_basename,'mms_',int2str(data.mms_type));
 else
@@ -30,8 +41,8 @@ end
 result_basename
 data.vtk_basename        = result_basename;
 
-data.save_workspace      = false;
-data.workspace_name      = result_basename;
+data.save_workspace      = true;
+data.workspace_name      = strcat(result_basename,'.mat');
 
 pwld_solve_problem(data);
 
